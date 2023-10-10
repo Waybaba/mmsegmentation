748861729902e23369914bdf158b225a62fc9d22
waybaba <wwang828@uwo.ca>
2023-10-10 15:05:21 +0000

    amlt

--- src/city2city_cfg.py
+++ src/city2city_cfg.py
@@ -1,48 +1,32 @@
 _base_ = [
-    # '../configs/_base_/models/deeplabv3_r50-d8.py', 
-    '../configs/_base_/models/pspnet_r50-d8.py',
-    # '../configs/_base_/datasets/cityscapes.py',
-    '../configs/_base_/default_runtime.py', 
-    '../configs/_base_/schedules/schedule_40k.py'
+    '.gtatrain_cfg.py',
 ]
-
-# runtime
-
-default_hooks = dict(
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    visualization=dict(type='SegVisualizationHook')
-)
 
 
 # dataset GTA->Cityscapes
 # import os
 # data_folder = os.environ['UDATADIR']
 data_folder = "/data" # ! CHANGE for dataset path
-tags = ["a_test"]
-crop_size = (512, 512) # ! Crop size
+crop_size = (512, 1024) # ! Crop size
 data_preprocessor = dict(size=crop_size)
 model = dict(data_preprocessor=data_preprocessor)
+crop_size = (512, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
     dict(
         type='RandomResize',
-        scale=(1280,720), # ! from daformer settings
+        scale=(2048, 1024),
         ratio_range=(0.5, 2.0),
         keep_ratio=True),
-    # dict(type='Resize', scale=(1280, 720)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0.5), # ! ? why this is no normization
+    dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
     dict(type='PackSegInputs')
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile'), # ! TODO do we need to keep as train size
-    dict(type='Resize', scale=(1024, 512), keep_ratio=True), # ! why, the origin is 2048, 1024
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1024, 512), keep_ratio=True),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
     dict(type='LoadAnnotations'),
@@ -70,14 +54,14 @@ train_dataloader = dict(
     persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
     dataset=dict(
-        type="GTADataset",
-        data_root=data_folder+"/GTA5",
+        type="CityscapesDataset",
+        data_root=data_folder+"/Cityscapes/",
         data_prefix=dict(
-            img_path='images', seg_map_path='labels'),
+            img_path='leftImg8bit/train', seg_map_path='gtFine/train'),
         pipeline=train_pipeline))
 val_dataloader = dict(
-    batch_size=2,
-    num_workers=2,
+    batch_size=1,
+    num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
@@ -87,6 +71,3 @@ val_dataloader = dict(
             img_path='leftImg8bit/val', seg_map_path='gtFine/val'),
         pipeline=test_pipeline))
 test_dataloader = val_dataloader
-
-val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
-test_evaluator = val_evaluator
