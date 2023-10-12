6ce58ed8bad93c804354407e6f81701f1eb8e0ee
waybaba <wwang828@uwo.ca>
2023-10-12 09:08:15 +0000

    cfg g2c

---
+++ src/cfgs/c2c_daformer_b0.py
@@ -0,0 +1,48 @@
+_base_ = [
+    # '../../_base_/models/segformer_mit-b0.py',
+	# '../../configs/_base_/models/daformer_aspp_mitb5.py',
+    '../../configs/_base_/models/daformer_aspp_mitb0.py',
+    '../../configs/_base_/datasets/cityscapes_1024x1024.py',
+    '../../configs/_base_/default_runtime.py', 
+	'../../configs/_base_/schedules/schedule_160k.py'
+]
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+# checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b0_20220624-7e0fe6dd.pth'  # noqa
+checkpoint = '/data/models/SegFormer/pretrained_models_ImageNet/mit_b0.pth'
+# checkpoint = '/data/models/SegFormer/pretrained_models_ImageNet/mit_b5.pth'
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(init_cfg=dict(type='Pretrained', checkpoint=checkpoint)),
+    # test_cfg=dict(mode='slide', crop_size=crop_size, stride=(768, 768))
+    test_cfg=dict(mode='whole') # segformer
+)
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=160000,
+        by_epoch=False,
+    )
+]
+
+train_dataloader = dict(batch_size=1, num_workers=4)
+val_dataloader = dict(batch_size=1, num_workers=4)
+test_dataloader = val_dataloader
--- src/cfgs/c2c_segformer.py
+++ src/cfgs/c2c_segformer.py
@@ -1,3 +1,3 @@
 _base_ = [
-	'../../configs/segformer/segformer_mit-b0_8xb1-160k_cityscapes-1024x1024.py'
+	'../../configs/segformer/segformer_mit-b5_8xb1-160k_cityscapes-1024x1024.py'
 ]
