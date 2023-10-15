911e8a42504857754bbd2c90dc253c445db5a974
waybaba <wwang828@uwo.ca>
2023-10-15 08:30:10 +0000

    pseudo label

--- src/ttda.py
+++ src/ttda.py
@@ -4,6 +4,8 @@ import torch
 from mmengine.hooks import Hook
 from mmengine.runner import Runner
 
+from mmengine.config import Config, DictAction
+from copy import deepcopy
 from mmseg.registry import HOOKS
 from mmengine.model import (MMDistributedDataParallel, convert_sync_batchnorm,
                             is_model_wrapper, revert_sync_batchnorm)
@@ -22,8 +24,9 @@ class TTDAHook(Hook):
             Default: 50.
     """
 
-    def __init__(self, turn_on_adapt=False) -> None:
+    def __init__(self, turn_on_adapt=False, **kwargs) -> None:
         self.turn_on_adapt = turn_on_adapt
+        self.kwargs = Config(kwargs)
 
     def fake_train_init(self, runner):
         """ Since we need to init something before test
@@ -97,6 +100,52 @@ class TTDAHook(Hook):
         # runner.call_hook('after_run')
         return None
 
+    @torch.enable_grad()
+    def ttda_before_test(self, runner, batch_idx, data_batch):
+        """
+        input:
+            inputs: 
+                inputs [(CH, 512, 1024)], data_samples [SegDataSample]
+                {
+                    gt_sem_seg: (512, 1024)
+                    img_path: 
+                    metainfo:
+                        scale_factor:
+                        ori_shape:
+                        img_shape:
+                        reduce_zero_label:
+                    img_shape:
+                    ori_shape:
+                    pred_sem_seg: # would have value after model.test_step
+                    seg_logits: # would have value after model.test_step
+                    scale_factor:
+                }
+        """
+        # init
+        if not self.turn_on_adapt: return
+        USE_PSEUDO_LABEL = True
+        inputs, data_samples = data_batch["inputs"], data_batch["data_samples"]
+
+        # inference label
+        data_samples_ = runner.model.test_step(data_batch) # data_samples_[0].pred_sem_seg.shape = (512, 1024)
+
+        # set data_batch_for_adapt for train
+        data_batch_for_adapt = deepcopy(data_batch)
+        if USE_PSEUDO_LABEL:
+            for i in range(len(data_samples)):
+                data_batch_for_adapt["data_samples"][i].gt_sem_seg = data_samples_[i].pred_sem_seg
+
+        ### adapt
+        optim_wrapper = runner.optim_wrapper
+        model = runner.model
+        with optim_wrapper.optim_context(model):
+            data_batch_for_adapt = model.data_preprocessor(data_batch_for_adapt, True)
+            losses = model._run_forward(data_batch_for_adapt, mode='loss')  # type: ignore
+        parsed_losses, log_vars = model.parse_losses(losses)  # type: ignore
+        optim_wrapper.update_params(parsed_losses)
+    
+    ### hooks
+    
     def before_run(self, runner) -> None:
         if not self.turn_on_adapt: return
         # if self.mode == "test":
@@ -106,7 +155,6 @@ class TTDAHook(Hook):
         # else:
         #     raise ValueError(f"mode {self.mode} not supported, only 'test' for TTDA ")
 
-    @torch.enable_grad()
     def before_test_iter(self,
                          runner: Runner,
                          batch_idx: int,
@@ -120,16 +168,8 @@ class TTDAHook(Hook):
                 Defaults to None.
             outputs (dict, Optional): Outputs from model. Defaults to None.
         """
-        if not self.turn_on_adapt: return
-        optim_wrapper = runner.optim_wrapper
-        inputs, data_samples = data_batch["inputs"], data_batch["data_samples"]
-        model = runner.model
-        with optim_wrapper.optim_context(model):
-            data_batch = model.data_preprocessor(data_batch, True)
-            losses = model._run_forward(data_batch, mode='loss')  # type: ignore
-        parsed_losses, log_vars = model.parse_losses(losses)  # type: ignore
-        optim_wrapper.update_params(parsed_losses)
-    
+        self.ttda_before_test(runner, batch_idx, data_batch)
+
     def after_test_epoch(self, runner, metrics):
         # runner.logger.info('after test epoch')
         # revert_sync_batchnorm(runner.model) # ! ??? shoud we handle?
@@ -137,3 +177,11 @@ class TTDAHook(Hook):
         runner.visualizer.add_scalars({
             "test/"+k: v for k, v in metrics.items()
         })
+
+    def after_test_iter(self, runner, batch_idx, data_batch, outputs):
+        # metrics = runner.test_evaluator.evaluate(len(runner.test_dataloader.dataset))
+        # runner.visualizer.add_scalars({
+        #     "test_step/"+k: v for k, v in metrics.items()
+        # })
+        # runner.logger.info(f"test iter {batch_idx} done, metrics: {metrics}")
+        pass
